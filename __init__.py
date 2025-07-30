80b24aafe9b001190056653e86ea9678af8db9ef
Rodrigo Fischer <rodrfischer@gmail.com>
2025-07-30 17:21:28 +0200

    gitignore

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from torch_utils import *
+from .torch_utils import Freeze, SequentialNet, ParallelNet, SurrogateNet, process_parameters, parameters, clone, entropy_reg, l1_reg
 
 __all__ = [Freeze, SequentialNet, ParallelNet, SurrogateNet, process_parameters, parameters, clone, entropy_reg, l1_reg]
